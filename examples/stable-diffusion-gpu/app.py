f3e8198942e7ea8eda9385405f883564de332ef5
Jordan Leigh <AlwaysBCoding@gmail.com>
2023-02-15 21:19:11 -0500

    updated run script

--- examples/stable-diffusion-gpu/app.py
+++ examples/stable-diffusion-gpu/app.py
@@ -2,26 +2,26 @@ import beam
 
 # The environment your code will run on
 app = beam.App(
-    name="stable-diffusion-app",
-    cpu=8,
-    memory="32Gi",
-    gpu="A10G",
-    python_version="python3.8",
-    python_packages=[
-        "diffusers[torch]>=0.10",
-        "transformers",
-        "torch",
-        "pillow",
-        "accelerate",
-        "safetensors",
-    ],
+  name="stable-diffusion-app",
+  cpu=8,
+  memory="32Gi",
+  gpu="A10G",
+  python_version="python3.8",
+  python_packages=[
+    "diffusers[torch]>=0.10",
+    "transformers",
+    "torch",
+    "pillow",
+    "accelerate",
+    "safetensors",
+  ],
 )
 
 # Deploys function as async webhook
-app.Trigger.Webhook(
-    inputs={"prompt": beam.Types.String()},
-    handler="run.py:generate_image",
-)
+# app.Trigger.Webhook(
+#   inputs={"prompt": beam.Types.String()},
+#   handler="run.py:generate_image",
+# )
 
 # File to store image outputs
 app.Output.File(path="output.png", name="myimage")
